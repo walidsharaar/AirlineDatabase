d7d08dc002d0d9966e127dfc1a99535e8d02990a
Sharaar <walidsharar@gmail.com>
2022-04-13 14:06:24 +0200

    Create Database

---
+++ CreateDatabase.sql
@@ -0,0 +1,12 @@
+-- Database: Airlines
+
+-- DROP DATABASE IF EXISTS "Airlines";
+
+CREATE DATABASE "Airlines"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_British Virgin Islands.1252'
+    LC_CTYPE = 'English_British Virgin Islands.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
