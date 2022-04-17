02369922160d840de1b547d81b7d6140290cf321
Sharaar <walidsharar@gmail.com>
2022-04-17 09:30:01 +0200

    Aggregations challenge

---
+++ aggregations.sql
@@ -0,0 +1,4 @@
+-- Determine how many flights from each city to other cities, return the the name of city and count of flights more than 50 order the data from the largest no of flights to the least?
+
+
+-- Return all flight details in the indicated day 2017-08-28 include flight count ascending order and departures count and when departures happen in arrivals count and when arrivals happen?
