d1251513541be3d22f00ee39ad682e4aa54547e6
Sharaar <walidsharar@gmail.com>
2022-04-17 10:26:03 +0200

    Conditional Statement Challenge

---
+++ SqlConditions.sql
@@ -0,0 +1,2 @@
+-- SQL conditional Statements
+-- write a query to arrange the range of model of air crafts so  Short range is less than 2000, Middle range is more than 2000 and less than 5000 & any range above 5000 is long range?
