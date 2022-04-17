a14473388902ca3c6c8f4125a895ca1b540761e0
Sharaar <walidsharar@gmail.com>
2022-04-17 10:34:25 +0200

    Challenge 1 resolved

--- SqlConditions.sql
+++ SqlConditions.sql
@@ -1,2 +1,16 @@
 -- SQL conditional Statements
 -- write a query to arrange the range of model of air crafts so  Short range is less than 2000, Middle range is more than 2000 and less than 5000 & any range above 5000 is long range?
+
+SELECT model, range, 
+CASE WHEN range <2000 THEN 'Short'
+     WHEN range <5000 THEN 'Middle'
+     ELSE 'Long '
+     END AS range
+FROM aircrafts
+ORDER BY model;
+
+
+
+---What is the shortest flight duration for each possible flight from Moscow to St. Petersburg, and how many times was the flight delayed for more than an hour?
+
+select * from tickets;
