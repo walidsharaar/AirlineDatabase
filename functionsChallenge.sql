3e99cde84fbc3cc3ffb10897097f64c9d815f826
Sharaar <walidsharar@gmail.com>
2022-04-20 06:54:31 +0200

    Challenge 3 is completed

--- functionsChallenge.sql
+++ functionsChallenge.sql
@@ -23,3 +23,7 @@ FROM tickets
 GROUP BY 1
 ORDER BY 2 DESC;
 --Which combinations of first names and last names separately occur most often? What is the ratio of the passengers with such names to the total number of passengers?
+WITH p AS (SELECT left(passenger_name, position(' ' IN passenger_name)) AS passenger_name FROM tickets)
+SELECT passenger_name, round( 100.0 * cnt / sum(cnt) OVER (), 2) AS percent
+FROM (SELECT passenger_name,count(*) cnt FROM p GROUP BY passenger_name) t
+ORDER BY percent DESC;
