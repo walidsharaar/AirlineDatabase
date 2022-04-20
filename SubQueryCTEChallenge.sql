846f426362b2745e09de7ac2a6b6fe2a15ce76a1
Sharaar <walidsharar@gmail.com>
2022-04-20 06:36:53 +0200

    Challenge 3 is completed

--- SubQueryCTEChallenge.sql
+++ SubQueryCTEChallenge.sql
@@ -1,16 +1,22 @@
 --How many people can be included into a single booking according to the available data?
-
-
---Which combinations of first and last names occur most often? What is the ratio of the passengers with such names to the total number of passengers?
 SELECT tt.bookings_no,count(*)passengers_no
 FROM (SELECT t.book_ref, count(*) bookings_no FROM tickets t GROUP BY t.book_ref) tt
 GROUP BY tt.bookings_no
 ORDER BY tt.bookings_no;
 
---What are the maximum and minimum ticket prices in all directions?
+--Which combinations of first and last names occur most often? What is the ratio of the passengers with such names to the total number of passengers?
 SELECT passenger_name, round( 100.0 * cnt / sum(cnt) OVER (), 2) AS percent
 FROM (SELECT passenger_name, count(*) cnt  FROM tickets GROUP BY passenger_name) sub
 ORDER BY percent DESC;
+
+--What are the maximum and minimum ticket prices in all directions?
+
+SELECT (SELECT city ->> 'en' FROM airports WHERE airport_code = f.departure_airport) AS departure_city, (SELECT city ->> 'en' FROM airports WHERE airport_code = f.arrival_airport) AS arrival_city, max (tf.amount), min (tf.amount)
+FROM flights f
+JOIN ticket_flights tf
+ON f.flight_id = tf.flight_id
+GROUP BY 1, 2
+ORDER BY 1, 2;
 
 --Get a list of airports in cities with more than one airport ?
 
