87828fcf684ee03c1473027f2db70fcad7425780
Sharaar <walidsharar@gmail.com>
2022-04-20 06:50:08 +0200

    Challenge 1 is completed

--- functionsChallenge.sql
+++ functionsChallenge.sql
@@ -6,7 +6,15 @@ CREATE VIEW cities AS SELECT (SELECT city ->> 'en' FROM airports
 (SELECT city ->> 'en' FROM airports 
  WHERE airport_code =arrival_airport) 
 AS arrival_city
-FROM flights
+FROM flights;
+
+-- By using the view with can proceed the challenge
+SELECT departure_city, count (*)
+FROM cities
+GROUP BY departure_city
+HAVING departure_city IN (SELECT city->> 'en' FROM airports )
+ORDER BY count DESC;
+
 
 --Suppose our airline marketers want to know how often there are different names among the passengers?
 
