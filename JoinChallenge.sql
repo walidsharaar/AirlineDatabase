72ed133f9b8b1b2a0a42033e7731bb0a53ad2054
Sharaar <walidsharar@gmail.com>
2022-04-20 05:28:05 +0200

    Challenge 1 completed

--- JoinChallenge.sql
+++ JoinChallenge.sql
@@ -1,5 +1,16 @@
 -- Who traveled from Moscow (SVO) to Novosibirsk (OVB) on seat 1A yesterday, and when was the ticket booked?
 
+SELECT t.passenger_name, b.book_date
+FROM bookings b
+JOIN tickets t
+ON t.book_ref = b.book_ref
+JOIN boarding_passes bp
+ON bp.ticket_no = t.ticket_no
+JOIN flights f
+ON f.flight_id = bp.flight_id
+WHERE f.departure_airport = 'SVO' AND f.arrival_airport = 'OVB'
+AND f.scheduled_departure::date = public.now()::date - INTERVAL '2 day'
+AND bp.seat_no = '1A';
 
 --Find the most disciplined passengers who checked in first for all their flights. Take into account only those passengers who took at least two flights ?
 
