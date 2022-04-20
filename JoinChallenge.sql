c98390ce9ab771bedb5ea43b1d4377426656512a
Sharaar <walidsharar@gmail.com>
2022-04-20 05:25:09 +0200

    Challenge for better understanding joins

---
+++ JoinChallenge.sql
@@ -0,0 +1,12 @@
+-- Who traveled from Moscow (SVO) to Novosibirsk (OVB) on seat 1A yesterday, and when was the ticket booked?
+
+
+--Find the most disciplined passengers who checked in first for all their flights. Take into account only those passengers who took at least two flights ?
+
+
+
+--Calculate the number of passengers and number of flights departing from one airport (SVO) during each hour on the indicated day 2017-08-02 ?
+
+
+
+--Use SQL  joins to  return unique city name, flight_no, airport and timezone?
