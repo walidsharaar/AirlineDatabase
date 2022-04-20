5113309342727a7bbf75768efbc4599339685c34
Sharaar <walidsharar@gmail.com>
2022-04-20 06:11:25 +0200

    Sub query and Common Table Expression Challenge

---
+++ SubQueryCTEChallenge.sql
@@ -0,0 +1,13 @@
+--How many people can be included into a single booking according to the available data?
+
+
+--Which combinations of first and last names occur most often? What is the ratio of the passengers with such names to the total number of passengers?
+
+
+--What are the maximum and minimum ticket prices in all directions?
+
+
+--Get a list of airports in cities with more than one airport ?
+
+
+--What will be the total number of different routes that are theoretically can be laid between all cities?
