a68e4fe9f716f21d30697dddd4058a632c16b9a1
Sharaar <walidsharar@gmail.com>
2022-04-20 06:46:00 +0200

    Challenge on working function with SQL

---
+++ functionsChallenge.sql
@@ -0,0 +1,7 @@
+--Count the number of routes laid from the airports?
+
+
+--Suppose our airline marketers want to know how often there are different names among the passengers?
+
+
+--Which combinations of first names and last names separately occur most often? What is the ratio of the passengers with such names to the total number of passengers?
