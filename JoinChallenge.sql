d5e66f4cf2383b421ca49e6dcd754c1ea702263b
Sharaar <walidsharar@gmail.com>
2022-04-20 05:29:59 +0200

    Challenge 2 is done

--- JoinChallenge.sql
+++ JoinChallenge.sql
@@ -14,7 +14,12 @@ AND bp.seat_no = '1A';
 
 --Find the most disciplined passengers who checked in first for all their flights. Take into account only those passengers who took at least two flights ?
 
-
+SELECT t.passenger_name, t.ticket_no
+FROM tickets t
+JOIN boarding_passes bp
+ON bp.ticket_no = t.ticket_no
+GROUP BY t.passenger_name, t.ticket_no
+HAVING max(bp.boarding_no) = 1 AND count(*) > 1;
 
 --Calculate the number of passengers and number of flights departing from one airport (SVO) during each hour on the indicated day 2017-08-02 ?
 
