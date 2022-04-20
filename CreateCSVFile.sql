cded2d3a911cd624021828ec1cb0208ca5c8f988
Sharaar <walidsharar@gmail.com>
2022-04-20 08:19:46 +0200

    Generate CSV file from different table into CSV

---
+++ CreateCSVFile.sql
@@ -0,0 +1,13 @@
+copy (select passenger_name, DATE_PART('day', book_date) as day, DATE_PART('month',book_date) as month,
+	  (select city from airports where airport_code = f.departure_airport)as departure_city,
+	  (select city from airports where airport_code = f.arrival_airport)as arrival_city,
+	  sum(total_amount) as sales
+	  from bookings b
+	  join tickets t
+	  on b.book_ref = t.book_ref
+	  join ticket_flights tf
+	  on t.ticket_no = tf.ticket_no
+	  join flights f
+	  on tf.flight_id = f.flight_id
+	  group by 1,2,3,4,5
+	  order by 3,6) to 'd:\csv\airlines_data_part.csv' DELIMITER ',' CSV HEADER;
