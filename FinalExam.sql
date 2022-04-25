629dd1283d6dba97f50422527cce44524b6c903a
Sharaar <walidsharar@gmail.com>
2022-04-25 12:44:26 +0200

    Solution of Question 2

--- FinalExam.sql
+++ FinalExam.sql
@@ -16,3 +16,10 @@ WINDOW w AS (PARTITION BY tf.ticket_no
 ORDER BY f.scheduled_departure);
 
 --2. Find the most disciplined passengers who checked in first for all their flights. Take into account only those passengers who took at least two flights?
+
+SELECT t.passenger_name, t.ticket_no
+FROM tickets t
+JOIN boarding_passes bp
+ON bp.ticket_no = t.ticket_no
+GROUP BY t.passenger_name,t.ticket_no
+HAVING max(bp.boarding_no) = 1 AND count(*) > 1;
