467201201ece627289e796d29e76695cb6e60275
Sharaar <walidsharar@gmail.com>
2022-04-25 12:50:50 +0200

    final question solution

--- FinalExam.sql
+++ FinalExam.sql
@@ -47,7 +47,8 @@ FROM (SELECT s.seat_no FROM  seats s
   WHERE flight_no = 'PG0404'                                     
   AND scheduled_departure::date = public.now()::date - INTERVAL '1 day')) t; 
 
-
-
-
 --5. what is the different between the tables created using VIEWS and the tables created using SELECT INTO ?
+
+* SELECT INTO created a table that is structured with a set number of columns and a boundless number of columns
+so you can apply all the SQL querying commands on the the resulted table.
+* But VIEWS is treated as  virtual tables that is extracted from a database So you cannot apply all the SQL querying commands on the the resulted table as joins.
