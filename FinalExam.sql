6688ea74b2e40c70f7ac2479abf1feffb64ab9cd
Sharaar <walidsharar@gmail.com>
2022-04-25 12:42:13 +0200

    Solution for Question 1

--- FinalExam.sql
+++ FinalExam.sql
@@ -1,3 +1,16 @@
 -- Final Exam and Recap
 
 --1. For each ticket, display all the included flight segments, together with connection time. Limit the result to the tickets booked a week ago?
+SELECT tf.ticket_no, f.departure_airport, f.arrival_airport, f.scheduled_arrival,
+lead(f.scheduled_departure) OVER w AS next_departure,
+lead(f.scheduled_departure) OVER w - f.scheduled_arrival AS gap
+FROM bookings b
+JOIN tickets t
+ON t.book_ref = b.book_ref
+JOIN ticket_flights tf
+ON tf.ticket_no = t.ticket_no
+JOIN flights f
+ON tf.flight_id = f.flight_id
+WHERE b.book_date = public.now()::date - INTERVAL '7 day'
+WINDOW w AS (PARTITION BY tf.ticket_no
+ORDER BY f.scheduled_departure);
